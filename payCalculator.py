2d74a7e12b0f37cfb4aced9c28d2e0b357a8e24d
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-01-26 16:07:11 -0600

    Helping gf with hers and noticed mine didn't cast inputs. Updated.

--- payCalculator.py
+++ payCalculator.py
@@ -1,9 +1,10 @@
 def calculatePay():
     # Implement your solution in between the two comment blocks
+    print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-    rate = input("Enter Rate:")
-    print("calculating pay\n"+str(hrs*rate))  
+    hrs = int(input("Enter Hours:"))
+    rate = float(input("Enter Rate:"))
+    print(hrs*rate)  
     # end assignment
 
 
